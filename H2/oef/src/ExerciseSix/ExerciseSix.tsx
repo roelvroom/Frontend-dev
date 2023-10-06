2e319fc1fd82b8822035785e320b20e5e6a373f6
Roel Vroom <r0950679@student.thomasmore.be>
2023-10-06 11:08:23 +0200

    first commit

---
+++ H2/oef/src/ExerciseSix/ExerciseSix.tsx
@@ -0,0 +1,14 @@
+import {FunctionComponent} from 'react'
+
+interface ExerciseSixProps {
+
+}
+
+const ExerciseSix: FunctionComponent<ExerciseSixProps> = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default ExerciseSix
