eb058d40e4842b511287d286fcc796c76e421867
Roel Vroom <r0950679@student.thomasmore.be>
2023-10-18 16:03:47 +0200

    ExerciseSix

--- H2/oefReact/src/App.tsx
+++ H2/oefReact/src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'  
 import Exercise from './Exercise/Exercise'
+import ExerciseSix from './ExerciseSix/ExerciseSix'
 
 function App() {
 
   return (
     <>
       <Exercise title='Exercise 6: Calculator'>
-
+        <ExerciseSix/>
       </Exercise>
     </>
   )
